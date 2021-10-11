edbac5f6ef22aac1721159e80b7657e95a470a81
Vitaliy Tikhanov <love-vitaliy@ya.ru>
2021-10-11 17:23:02 +0300

    added sidebar info

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,82 +1,54 @@
 <div class="sidebar">
-{{--    <div class="widget-no-style">--}}
-{{--        <div class="newsletter-widget text-center align-self-center">--}}
-{{--            <h3>Subscribe Today!</h3>--}}
-{{--            <p>Subscribe to our weekly Newsletter and receive updates via email.</p>--}}
-{{--            <form class="form-inline" method="post">--}}
-{{--                <input type="text" name="email" placeholder="Add your email here.." required="" class="form-control">--}}
-{{--                <input type="submit" value="Subscribe" class="btn btn-default btn-block">--}}
-{{--            </form>--}}
-{{--        </div><!-- end newsletter -->--}}
-{{--    </div>--}}
+    {{--    <div class="widget-no-style">--}}
+    {{--        <div class="newsletter-widget text-center align-self-center">--}}
+    {{--            <h3>Subscribe Today!</h3>--}}
+    {{--            <p>Subscribe to our weekly Newsletter and receive updates via email.</p>--}}
+    {{--            <form class="form-inline" method="post">--}}
+    {{--                <input type="text" name="email" placeholder="Add your email here.." required="" class="form-control">--}}
+    {{--                <input type="submit" value="Subscribe" class="btn btn-default btn-block">--}}
+    {{--            </form>--}}
+    {{--        </div><!-- end newsletter -->--}}
+    {{--    </div>--}}
 
-    <div class="widget">
-        <h2 class="widget-title">Recent Posts</h2>
-        <div class="blog-list-widget">
-            <div class="list-group">
-                <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 justify-content-between">
-                        <img src="upload/small_07.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
-                        <small>12 Jan, 2016</small>
-                    </div>
-                </a>
+    @if($popular_posts)
+        <div class="widget">
+            <h2 class="widget-title">Recent Posts</h2>
+            <div class="blog-list-widget">
+                <div class="list-group">
+                    @foreach($popular_posts as $popular_post)
 
-                <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 justify-content-between">
-                        <img src="upload/small_08.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">Let's make an introduction for creative life</h5>
-                        <small>11 Jan, 2016</small>
-                    </div>
-                </a>
+                        <a href="{{ route('posts.single',['slug'=>$popular_post->slug]) }}"
+                           class="list-group-item list-group-item-action flex-column align-items-start">
+                            <div class="w-100 justify-content-between">
+                                <img src="{{ $popular_post->getImage() }}" alt="" class="img-fluid float-left">
+                                <h5 class="mb-1">{{ $popular_post->title }}</h5>
+                                <small>
+                                    {{ $popular_post->getPostDate() }}
+                                    <i class="fa fa-eye"></i>
+                                    {{ $popular_post->views }}
+                                </small>
+                            </div>
+                        </a>
+                    @endforeach
 
-                <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="w-100 last-item justify-content-between">
-                        <img src="upload/small_09.jpg" alt="" class="img-fluid float-left">
-                        <h5 class="mb-1">Did you see the most beautiful sea in the world?</h5>
-                        <small>07 Jan, 2016</small>
-                    </div>
-                </a>
-            </div>
-        </div><!-- end blog-list -->
-    </div><!-- end widget -->
-
-    <div id="" class="widget">
-        <h2 class="widget-title">Advertising</h2>
-        <div class="banner-spot clearfix">
-            <div class="banner-img">
-                <img src="upload/banner_03.jpg" alt="" class="img-fluid">
-            </div><!-- end banner-img -->
-        </div><!-- end banner -->
-    </div><!-- end widget -->
-
-    <div class="widget">
-        <h2 class="widget-title">Instagram Feed</h2>
-        <div class="instagram-wrapper clearfix">
-            <a class="" href="#"><img src="upload/small_09.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_01.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_02.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_03.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_04.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_05.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_06.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_07.jpg" alt="" class="img-fluid"></a>
-            <a href="#"><img src="upload/small_08.jpg" alt="" class="img-fluid"></a>
-        </div><!-- end Instagram wrapper -->
-    </div><!-- end widget -->
-
-    <div class="widget">
-        <h2 class="widget-title">Popular Categories</h2>
-        <div class="link-widget">
-            <ul>
-                <li><a href="#">Marketing <span>(21)</span></a></li>
-                <li><a href="#">SEO Service <span>(15)</span></a></li>
-                <li><a href="#">Digital Agency <span>(31)</span></a></li>
-                <li><a href="#">Make Money <span>(22)</span></a></li>
-                <li><a href="#">Blogging <span>(66)</span></a></li>
-                <li><a href="#">Entertaintment <span>(11)</span></a></li>
-                <li><a href="#">Video Tuts <span>(87)</span></a></li>
-            </ul>
-        </div><!-- end link-widget -->
-    </div><!-- end widget -->
+                </div>
+            </div><!-- end blog-list -->
+        </div><!-- end widget -->
+    @endif
+    @if($popular_categories)
+        <div class="widget">
+            <h2 class="widget-title">Popular Categories</h2>
+            <div class="link-widget">
+                <ul>
+                    @foreach($popular_categories as $popular_category)
+                        <li>
+                            <a href="{{ route('categories.single', ['slug'=>$popular_category->slug]) }}">{{ $popular_category->title }}
+                                <span>({{ $popular_category->posts_count }})</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div><!-- end link-widget -->
+        </div><!-- end widget -->
+    @endif
 </div><!-- end sidebar -->
